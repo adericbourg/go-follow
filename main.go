efc76fa94b29f712b4e7c4324c50017277d75bf8
Alban Dericbourg <a.dericbourg@criteo.com>
2018-05-05 12:23:29 +0200

    Add comment and favorite

--- main.go
+++ main.go
@@ -36,8 +36,9 @@ func main() {
 	}
 
 	context := Context{
-		client,
-		time.Now().AddDate(-1, 0, 0),
+		Client:      client,
+		LastRetweet: time.Now().AddDate(-1, 0, 0),
+		LastComment: time.Now().AddDate(-1, 0, 0),
 	}
 
 	demux := twitter.NewSwitchDemux()
@@ -67,17 +68,28 @@ func getenv(key string) string {
 type Context struct {
 	Client      *twitter.Client
 	LastRetweet time.Time
+	LastComment time.Time
 }
 
 func handleTweet(tweet *twitter.Tweet, context *Context) {
 	if strings.HasPrefix(tweet.Text, "RT ") {
+		log.Printf("Ignore RT: %s", tweet.Text)
+		return
+	}
+	if tweet.InReplyToStatusID == 0 {
+		log.Printf("Ignore reply: %s", tweet.Text)
 		return
 	}
 
-	switch rand.Intn(1) {
+	switch rand.Intn(3) {
 	case 0:
 		retweet(tweet, context)
+	case 1:
+		comment(tweet, context)
+	case 2:
+		favorite(tweet, context)
 	default:
+		favorite(tweet, context)
 	}
 }
 
@@ -91,4 +103,32 @@ func retweet(tweet *twitter.Tweet, context *Context) {
 		})
 		context.LastRetweet = time.Now()
 	}
+}
+
+var Comments = []string{
+	"Thanks!",
+	"Thank you for this :)",
+	"Interesting. Thanks for sharing!",
+	"👍",
+}
+
+func comment(tweet *twitter.Tweet, context *Context) {
+	duration := time.Since(context.LastComment)
+	// 10min + random between 0 and 5min
+	if duration.Minutes() >= (10 + rand.Float64()*5) {
+		comment := Comments[ rand.Intn(len(Comments))]
+		log.Printf("Comment: '%s' to '%s'\n", comment, tweet.Text)
+		context.Client.Statuses.Update(comment, &twitter.StatusUpdateParams{
+			Status:            comment,
+			InReplyToStatusID: tweet.ID,
+		})
+		context.LastComment = time.Now()
+	}
+}
+
+func favorite(tweet *twitter.Tweet, context *Context) {
+	log.Printf("Favorite: %s\n", tweet.Text)
+	context.Client.Favorites.Create(&twitter.FavoriteCreateParams{
+		ID: tweet.ID,
+	})
 }
